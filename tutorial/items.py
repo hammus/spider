774714462dd30b5db8cb7d134e0604b5452e1147
Liam Whan <liam.whan@gmail.com>
2017-03-23 13:24:38 -0700

    RentADress

--- tutorial/items.py
+++ tutorial/items.py
@@ -20,3 +20,23 @@ class StarNowItem(scrapy.Item):
     chest = scrapy.Field()
     hips = scrapy.Field()
     weight = scrapy.Field()
+
+class RADItem(scrapy.Item):
+    name = scrapy.Field()
+    email = scrapy.Field()
+    facebook = scrapy.Field()
+    instagram = scrapy.Field()
+    phone = scrapy.Field()
+    brand_or_designer = scrapy.Field()
+    item_name = scrapy.Field()
+    city = scrapy.Field()
+    post_code = scrapy.Field()
+    street_address = scrapy.Field()
+    labeled_size = scrapy.Field()
+    rrp = scrapy.Field()
+    hire_price = scrapy.Field()
+    best_fit_size = scrapy.Field()
+    item_views = scrapy.Field()
+    date_posted = scrapy.Field()
+    description = scrapy.Field()
+    
