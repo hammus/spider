1c26fcfb198295fda542a7ea2168a43138e93b2d
Liam Whan <liam.whan@gmail.com>
2017-09-18 11:49:17 +1000

    PArse obfuscated

--- tutorial/spiders/rad_email.py
+++ tutorial/spiders/rad_email.py
@@ -4,6 +4,7 @@ import re
 import urllib
 from scrapy.utils.log import configure_logging
 
+
 try:
     from HTMLParser import HTMLParser
 except ImportError:
@@ -38,24 +39,24 @@ class RADItem(scrapy.Item):
 
 class RADSpider(scrapy.Spider):
 
-    name = "rad"
+    name = "rad_email"
     allowed_domains = ["rentadressaustralia.com"]
     start_urls = [
-        "http://rentadressaustralia.com/browse/Dresses/?page=1"
+        "http://rentadressaustralia.com/listing/14589/White-Reverie.html?searchId=59bb65bf6dcfc"
         ]
 
     def parse(self, response):
-        for href in response.xpath("//div[@class='thumbnail']/div[@class='caption']/h1/a/@href").extract():
-            yield scrapy.Request(response.urljoin(href), callback=self.parse_details)
+        # for href in response.xpath("//div[@class='thumbnail']/div[@class='caption']/h1/a/@href").extract():
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_details)
 
-        s = h.unescape(response.xpath("//a[@class='nextPageSelector']/@href").extract_first())
-        next_page = "".join(s.split())
-        print next_page
+        # s = h.unescape(response.xpath("//a[@class='nextPageSelector']/@href").extract_first())
+        # next_page = "".join(s.split())
         # print next_page
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
-
+        # # print next_page
+        # if next_page is not None:
+            # next_page = response.urljoin(next_page)
+        # yield scrapy.Request(response, callback=self.parse)
+        
 
 
     def parse_details(self, response):
@@ -66,15 +67,13 @@ class RADSpider(scrapy.Spider):
         # catNodes = response.xpath("//table[@id = 'ctl00_cphMain_physicalDetails_memberAttributes']//td")
 # //*[@id="summary"]/div/div/div[1]/dl/dd[6]/text() //*[@id="summary"]/div/div/div[1]/dl/dd[9]/span
         item = RADItem()
-        item["name"] = ' '.join(extract_xpath(response, "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/ul/li[1]/h4/text()")).strip()
-        try:
-            scriptText = ' '.join(extract_xpath(response, "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/ul/li[4]/span/script/text()")).strip()
-            emailHex = re.search('^eval\(unescape\(\'(.*)\'\)\)$', scriptText).group(1)
-            email = re.search("^document.write\(\'\<a\shref\=\"mailto:(.*)\"\s\>Send E-mail</a>'\);", urllib.unquote(emailHex)).group(1)
-            item["email"] = email
-        except AttributeError: 
-            print "No Email Detected"
-            item["email"] = ""
+        # item["name"] = ' '.join(extract_xpath(response, "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/ul/li[1]/h4/text()")).strip()
+        scriptText = ' '.join(extract_xpath(response, "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/ul/li[4]/span/script/text()")).strip()
+        emailHex = re.search('^eval\(unescape\(\'(.*)\'\)\)$', scriptText).group(1)
+        email = re.search("^document.write\(\'\<a\shref\=\"mailto:(.*)\"\s\>Send E-mail</a>'\);", urllib.unquote(emailHex)).group(1)
+
+
+        item["email"] = email
         item["phone"] = ' '.join(extract_xpath(response, "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/ul/li[3]/text()")).strip()
         item["facebook"] = ' '.join(extract_xpath(response, "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/ul/li[2]/a[1]/@href")).strip()
         item["instagram"] = ' '.join(extract_xpath(response, "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div[2]/ul/li[2]/a[2]/@href")).strip()
